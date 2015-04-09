dcb85f2e4e6e0e177eb0c472f8ff93d1edb8e9ff
Peter Hanely <hanelyp@gmail.com>
2015-04-09 11:02:01 -0700

    migrations fix.

--- migrations/acp.php
+++ migrations/acp.php
@@ -10,9 +10,9 @@ class acp extends \phpbb\db\migration\migration
 	// needed to prevent an activation crash, even if it does nothing.
 	public function effectively_installed()
 	{
-		// idiot core code is assuming something and not calling this.
+		// idiot core code is following migrations database field and not calling this.
 		//die;
-		echo 'effectively installed? ',$config['fancyDiceMacro_1'],'<br />';
+		//echo 'effectively installed? ',$config['fancyDiceMacro_1'],'<br />';
 		return false;
 		global $config;
 		return isset($config['fancyDiceMacro_1']);	//true;
@@ -26,7 +26,7 @@ class acp extends \phpbb\db\migration\migration
 // */
 	public function update_data()
 	{
-		echo "fetching update data<br />\n";
+		//echo "fetching update data<br />\n";
 		//die;
 		return array(
 			//array('config.add', array('acme_demo_goodbye', 0)),
@@ -54,20 +54,74 @@ class acp extends \phpbb\db\migration\migration
 			array('custom', array(array($this, 'add_bbcode'))),
 		);
 	}
+
+	// reflection on poor core design.
+	function bbcodecount()
+	{
+		$sql = 'SELECT MAX(bbcode_id) as max_bbcode_id
+							FROM ' . BBCODES_TABLE;
+		$result = $this->db->sql_query($sql);
+		$row = $this->db->sql_fetchrow($result);
+		$this->db->sql_freeresult($result);
+
+		if ($row)
+		{
+			$bbcode_id = $row['max_bbcode_id'] + 1;
+
+			// Make sure it is greater than the core bbcode ids...
+			if ($bbcode_id <= NUM_CORE_BBCODES)
+			{
+				$bbcode_id = NUM_CORE_BBCODES + 1;
+			}
+		}
+		else
+		{
+			$bbcode_id = NUM_CORE_BBCODES + 1;
+		}
+		return $bbcode_id;
+	}
+	
+	// brute force, but best option with opaque cache
+	function clearcache()
+	{
+		$cachedir = "cache";
+
+		$dir = opendir($cachedir);
+	// Delete everything but index.htm and .htaccess
+		while( $file = readdir( $dir ) )
+		{
+			//if ((strpos($file, '.php') > 0) && (strpos($file, 'sql_') === 0))
+			if (preg_match('#^sql_.*\.php$#', $file) )
+			{
+				unlink("$cachedir/$file");
+			}
+		}
+	}
 	
 	public function add_bbcode()
 	{
-		$sql_ary = array_merge($sql_ary, array(
-			'bbcode_tag'				=> 'dice',
-			'bbcode_match'				=> '#[dice\s+seed={number}\s+secure={simpletext}]{text}[/dice]#ie',
-			'bbcode_tpl'				=> '<blockquote>hanelyp\fancydice\bbcode::singlet()->replace_dice("{text}",{number},"{simpletext}")</blockquote>',
-			'display_on_posting'		=> 1,
-			'bbcode_helpline'			=> '[dice]3d6+1[/dice]',
-			'first_pass_match'			=> '#[dice](.+)[/dice]#ie',
-			'first_pass_replace'		=> 'hanelyp\fancydice\bbcode::singlet()->prep_dice("$1","$uid")',
-			'second_pass_match'			=> '[dice\s+seed=(\d+)\s+secure=(\w+)](.+)[/dice]#ie',
-			'second_pass_replace'		=> '<blockquote>hanelyp\fancydice\bbcode::singlet()->replace_dice("$3",$1,"$2")</blockquote>',
-		));
-		$this->db->sql_query('INSERT INTO ' . BBCODES_TABLE . $this->db->sql_build_array('INSERT', $sql_ary));
+		// double check for existing definition
+		$sql = 'SELECT * FROM ' . BBCODES_TABLE . " WHERE bbcode_tag = 'dice'";
+		$result = $this->db->sql_query($sql);
+		if (!$row = $this->db->sql_fetchrow($result))
+		{
+			$sql_ary = array(
+				'bbcode_id'					=> $this->bbcodecount(),
+				'bbcode_tag'				=> 'dice',
+				'bbcode_match'				=> '#[dice\s+seed={number}\s+secure={simpletext}]{text}[/dice]#ie',
+				'bbcode_tpl'				=> '<blockquote>hanelyp\fancydice\event\main_listener::singlet()->replace_dice("{text}",{number},"{simpletext}")</blockquote>',
+				'display_on_posting'		=> 1,
+				'bbcode_helpline'			=> '[dice]3d6+1[/dice]',
+				'first_pass_match'			=> '#\[dice\](.+)\[/dice\]#ie',
+				'first_pass_replace'		=> 'hanelyp\fancydice\event\main_listener::singlet()->prep_dice("$1","$uid")',
+				'second_pass_match'			=> '#\[dice\s+seed=(\d+)\s+secure=(\w+):?\w*\](.+)\[/dice\]#i',
+				'second_pass_replace'		=> 'hanelyp\fancydice\event\main_listener::singlet()->bb_replace_dice("$3",$1,"$2")',
+			);
+			$this->db->sql_query('INSERT INTO ' . BBCODES_TABLE . $this->db->sql_build_array('INSERT', $sql_ary));
+			// need to clear cache here
+			//$this->cache->destroy('sql', BBCODES_TABLE);
+			$this->clearcache();
+		}
+		$this->db->sql_freeresult($result);
 	}
 }
