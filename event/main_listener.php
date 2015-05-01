3a5ed152c6fa83bdb8f6f22868d9ac7668af1baf
Peter Hanely <hanelyp@gmail.com>
2015-05-01 12:27:44 -0700

    minor patch on seed generation.

--- event/main_listener.php
+++ event/main_listener.php
@@ -177,8 +177,8 @@ class main_listener implements EventSubscriberInterface
 		//return rand();
 		// random enough value that should never change over the course of making a post,
 		//	but always between posts
-		return $this->user->data['user_lastpost_time'] ^
-				1*$this->user->data['user_email_hash'] ^
+		return (1*$this->user->data['user_lastpost_time']) ^
+				(1*$this->user->data['user_email_hash']) ^
 				($this->config['fancyDiceSecure']*$this->rollcount++);
 	}
 	
